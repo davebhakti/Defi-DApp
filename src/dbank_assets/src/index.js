0bd6eb8b878d81eb8c1f543e3ad8e21914b7834e
davebhakti <bhakti@Bhakti.localdomain>
2025-08-07 00:00:21 -0700

    Solved the bugs as the older version could not handle the new data

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,23 +1,41 @@
 import { dbank } from "../../declarations/dbank";
 
-window.addEventListener("load", async function () {
-  try {
-    const currentAmount = await dbank.checkBalance();
-    document.getElementById("value").innerText =
-      Math.round(currentAmount * 100) / 100;
-  } catch (err) {
-    console.error("checkBalance failed:", err);
-  }
+window.addEventListener("load", async function() {
+  // console.log("Finished loading");
+  update();
 });
 
 document.querySelector("form").addEventListener("submit", async function(event) {
-    event.preventDefault();
-    //console.log("SUbmitted");
-    const inputAmount = parseFloat(document.getElementById("input-amount").value);
-    const outputAmount = parseFloat(document.getElementById("withdrawl-amount").value);
+  event.preventDefault();
+  // console.log("Submitted.");
 
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true);
+
+  if (document.getElementById("input-amount").value.length != 0) {
     await dbank.topUp(inputAmount);
+  }
 
-    const currentAmount = await dbank.checkBalance();
-    document.getElementById("value").innerText = Math.round(currentAmount + 100) / 100;
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await dbank.withdraw(outputAmount);
+  }
+
+  await dbank.compound();
+
+  update()
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled");
+
 });
+
+async function update() {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
